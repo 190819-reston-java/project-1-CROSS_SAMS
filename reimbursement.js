f156afd0535010ecf96d79e31140b3dc71ea9783
Nathan <alexcross17@gmail.com>
2019-09-28 19:47:24 -0400

    Can add new reimbursement

--- reimbursement.js
+++ reimbursement.js
@@ -2,20 +2,23 @@
 
 console.log("reimburse");
 
-const baseUrl = "/localproject1";
+const baseUrl = "/localproject1/p1Api";
 // const baseUrl = "http://localhost:8080/localproject1/reimbursements"
-const oUrl = `${baseUrl}/tdatabase`;
+// const oUrl = `${baseUrl}/tdatabase`;
+const oUrl = `${baseUrl}/reimbursements`;
 
 let newReimbursement = document.getElementById("reimbursementForm");
 let pendingReimbursements = document.getElementById("pendingReimbursements");
+let getPendingReimbursements = document.getElementById("get-pending-reimbursements");
+let updateReimbursement = document.getElementById("update-reimbursements")
 
 newReimbursement.addEventListener("submit", (event) => {
   event.preventDefault();
 
   console.log(reimbursementFromForm(newReimbursement));
 
-  fetch(baseUrl,
-    { method: "POST", body: JSON.stringify(reimbursementFromForm(reimbursementForm)) }
+  fetch(oUrl,
+    { method: "POST", body: JSON.stringify(reimbursementFromForm(newReimbursement)) }
   )
     .then((response) => {
       console.log(response);
@@ -26,55 +29,88 @@ newReimbursement.addEventListener("submit", (event) => {
       }
     })
     .catch(console.error);
+  // let reason = reimbursementForm.rType.value;
+  // let amount = reimbursementForm.rAmount.value;
+  // let date = reimbursementForm.rDate.value;
+
+  // console.log(`user selected ${reason}, ${amount}, ${date}`);
 });
 
-let reason = reimbursementForm.rType.value;
-let amount = reimbursementForm.rAmount.value;
-let date = reimbursementForm.rDate.value;
-
-console.log(`user selected ${reason}, ${amount}, ${date}`);
-
-getPendingObjects(reason, amount, date, (jsonString) => {
-  let pendingObjects = JSON.parse(jsonString);
-  console.log(pendingObjects);
-
-
-
-  pendingReimbursements.innerHTML = "";
-
-  for (let k in pendingObjects) {
-    let resultItem = document.createElement("li");
-    resultItem.innerText = `${k} : ${pendingObjects[k]}`;
-    pendingReimbursements.appendChild(resultItem);
-  }
-});
-
-
-function getPendingObjects(reason, amount, date, onSuccess) {
-  let xhr = new XMLHttpRequest();
-
-  xhr.addEventListener("readystatechange", () => {
-
-    if (xhr.readyState === 4) {
-      let response = xhr.response;
-      console.log(`Response received: ${response}`);
-
-
-      if (xhr.status >= 200 && xhr.status < 300) {
-        onSuccess(response);
-      } else {
-        console.error(`Failure with status ${xhr.status}`);
+getPendingReimbursements.addEventListener("click", (event) => {
+  fetch(oUrl, { method: "GET" })
+    .then((response) => {
+      return response.json();
+    })
+    .then((reimbursementsJson) => {
+      newDisplay();
+      for (let reimbursement in reimbursementsJson) {
+        console.log(reimbursementsJson[reimbursement]);
+        createLi(reimbursementsJson[reimbursement]);
       }
-    }
-  });
+    })
+    .catch(console.log)
+});
 
-  //       // prepares the request for sending
-  //       xhr.open("POST", `${baseUrl}/${reason}/${amount}/${date}`);
+updateReimbursement.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  //       // actually sends the request
-  //       xhr.send();
+  fetch(baseUrl,
+    { method: "PUT", body: JSON.stringify(reimbursementFromForm(updateReimbursement)) }
+  )
+    .then((response) => {
+      console.log(response);
+      if (response.status >= 200 && response.status < 300) {
+        alert("Reimbursement updated");
+      } else {
+        alert("Failed to update reimbursement");
+      }
+      updateReimbursement.hidden = true;
+    })
+    .catch(console.error);
+});
 
-}
+
+// getPendingReimbursements(reason, amount, date, (jsonString) => {
+//   let pendingReimbursements = JSON.parse(jsonString);
+//   console.log(pendingObjects);
+
+
+
+//   pendingReimbursements.innerHTML = "";
+
+//   for (let k in pendingOReimbursements) {
+//     let resultItem = document.createElement("li");
+//     resultItem.innerText = `${k} : ${pendingObjects[k]}`;
+//     pendingReimbursements.appendChild(resultItem);
+//   }
+//   // });
+
+
+//   function getPendingReimbursements(reason, amount, date, onSuccess) {
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.addEventListener("readystatechange", () => {
+
+//       if (xhr.readyState === 4) {
+//         let response = xhr.response;
+//         console.log(`Response received: ${response}`);
+
+
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//           onSuccess(response);
+//         } else {
+//           console.error(`Failure with status ${xhr.status}`);
+//         }
+//       }
+//     });
+
+//     // prepares the request for sending
+//     xhr.open("POST", `${baseUrl}/${reason}/${amount}/${date}`);
+
+//     // actually sends the request
+//     xhr.send();
+
+//   }
 
 
 let reimbursementFromForm = (form) => {
@@ -83,4 +119,21 @@ let reimbursementFromForm = (form) => {
   nReimbursement.amount = form.rAmount.value || "1000";
   nReimbursement.date = form.rDate.value || "1_1_2019";
   return nReimbursement;
+}
+
+let newDisplay = (reimbursement) => {
+  pendingReimbursements.innerHTML = "";
+};
+
+let createLi = (reimbursement) => {
+
+  let li = document.createElement("li");
+  li.innerText = `Reason: ${reimbursement.reason} Amount: ${reimbursement.amount} Date: ${reimbursement.date}`;
+  li.addEventListener("click", () => {
+    updateReimbursement.reason.value = reimbusement.reason;
+    updateReimbursement.amount.value = reimbusement.amount;
+    updateReimbursement.date.value = reimbusement.date;
+    newDisplay();
+  });
+  pendingReimbursements.append(li);
 }
