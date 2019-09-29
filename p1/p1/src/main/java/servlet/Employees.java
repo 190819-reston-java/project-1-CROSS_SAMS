a8d41f3ffaebd5aced4d2f0d734fcc962c588de9
edward sams <40345707+ewsams@users.noreply.github.com>
2019-09-29 10:24:34 -0400

    Add files via upload
    llatest p1

--- p1/p1/src/main/java/servlet/Employees.java
+++ p1/p1/src/main/java/servlet/Employees.java
@@ -42,10 +42,13 @@ public class Employees extends HttpServlet {
 	        response.getWriter().println("<title>Current Employees</title>");
 	        response.getWriter().println("</head>");
 	        response.getWriter().println("<body>");
-	        response.getWriter().println("<table>");
+	        response.getWriter().println("<table border=\"3\">");
 	        response.getWriter().println("<tr>");
+	        
 	        for (int i = 0; i < dao.totalEmployees().size(); i++) {
-	             response.getWriter().println("<td>" + dao.totalEmployees().get(i) + "</td>");
+	             response.getWriter().println("<td>" + dao.totalEmployees().get(i).getEmpoloyeeName()+ "</td>");
+	             response.getWriter().println("<td>" + dao.totalEmployees().get(i).getEmail()+ "</td>");
+	             response.getWriter().println("<td>" + dao.totalEmployees().get(i).getPhoneNumber()+ "</td>");
 	        }
 	        response.getWriter().println("</tr>");
 	        response.getWriter().println("</table>");
