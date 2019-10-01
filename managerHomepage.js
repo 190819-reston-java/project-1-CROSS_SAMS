c3ed933bc251eac05df3824583f2425118f7d1f8
Nathan <alexcross17@gmail.com>
2019-09-30 23:04:54 -0400

    managers can view all employees

--- managerHomepage.js
+++ managerHomepage.js
@@ -1,3 +1,71 @@
 'use strict';
 
 console.log("manager homepage test");
+
+const baseUrl = "/localproject1/p1Api";
+// const baseUrl = "http://localhost:8080/localproject1/reimbursements"
+// const oUrl = `${baseUrl}/tdatabase`;
+const oUrl = `${baseUrl}/employees`;
+
+let employeeList = document.getElementById("employee-list");
+let getEmployees = document.getElementById("get-employees");
+let employeeForm = document.getElementById("employee-form");
+
+getEmployees.addEventListener("click", (event) => {
+    fetch(oUrl, { method: "GET" })
+      .then((response) => {
+        return response.json();
+      })
+      .then((employeesJson) => {
+        newDisplay();
+        for (let employee in employeesJson) {
+          console.log(employeesJson[employee]);
+          createLi(employeesJson[employee]);
+        }
+      })
+      .catch(console.log)
+  });
+
+  employeeForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    fetch(baseUrl,
+      { method: "PUT", body: JSON.stringify(reimbursementFromForm(updateReimbursement)) }
+    )
+      .then((response) => {
+        console.log(response);
+        if (response.status >= 200 && response.status < 300) {
+          alert("Reimbursement updated");
+        } else {
+          alert("Failed to update reimbursement");
+        }
+        updateReimbursement.hidden = true;
+      })
+      .catch(console.error);
+  });
+
+  let employeeFromForm = (form) => {
+    let employee = {};
+    employee.reason = form.rType.value || "some reason";
+    employee.amount = form.rAmount.value || "1000";
+    employee.date = form.rDate.value || "1_1_2019";
+    employee.email = form.rEmail.value || "idk";
+    return employee;
+  }
+  
+  let newDisplay = (employee) => {
+    employeeList.innerHTML = "";
+  };
+  
+  let createLi = (employee) => {
+  
+    let li = document.createElement("li");
+    li.innerText = `Name: ${employee.name} | Email: $${employee.email} | Address: ${employee.address} | Password: ${employee.password} | Phone: ${employee.phone}`;
+    // li.addEventListener("click", () => {
+    //   updateReimbursement.reason.value = reimbusement.reason;
+    //   updateReimbursement.amount.value = reimbusement.amount;
+    //   updateReimbursement.date.value = reimbusement.date;
+    //   newDisplay();
+    // });
+    employeeList.append(li);
+  }
